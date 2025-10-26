432064bdfde900ed528b74a8d13ee900bd9709fa
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-26 15:45:03 +0100

    added 3 eventlisteners

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("Hello from script.js")
+document
+  .getElementById("tooLowButton")
+  .addEventListener("click", tooLowButtonClick);
+document
+  .getElementById("tooHighButton")
+  .addEventListener("click", tooHighButtonClick);
+document
+  .getElementById("correctButton")
+  .addEventListener("click", corectButtonClick);
+
+function tooLowButtonClick() {
+  alert("Hello from tooLow");
+}
+
+function tooHighButtonClick() {
+  alert("Hello from tooHigh");
+}
+
+function corectButtonClick() {
+  alert("Hello from Correct");
+}
